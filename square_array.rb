d9a0db11cb5f55f36e9cd822a71db8b88b1943f7
Hope Gipson <gipson.hope@gmail.com>
2020-06-22 16:32:34 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,9 @@
-
 def square_array(array)
-  array.collect!with_index { |square|
-    square = square * square
-  end
+  new_array = []
+  array.each do |square|
+  square = square * square
+  new_array << square
+
+end
+return new_array
 end
